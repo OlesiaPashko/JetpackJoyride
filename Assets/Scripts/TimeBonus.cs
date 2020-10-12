84b27268b16438dcd5320b5b353d640b005b6818
Olesia Pashko <pashkoolesia1234@gmail.com>
2020-10-12 14:00:07 +0300

    add time bonus and time logic

---
+++ Assets/Scripts/TimeBonus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBonus : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TimeManager timeManager = TimeManager.Instance;
+            timeManager.AddTime(30);
+            Debug.Log(timeManager.time);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -24,7 +24,7 @@ public class TimeManager : MonoBehaviour
 
     private void Start()
     {
-        time = 60f;
+        time = 1f;
     }
 
     private void Update()
